70fd2f50d33d722b6e6eaff970f2949649509000
Kaeden10 <Vanya200373@yandex.ru>
2020-03-04 13:45:56 +0300

    Функции теперь возвращают элементы (забыл об этом вчера)

--- remote.py
+++ remote.py
@@ -5,7 +5,7 @@ import requests
 def get_cities():
     r = requests.get('http://dt.miet.ru/ppo_it/api',
                      headers={'X-Auth-Token': '3zqb6xwartk4fga1'})
-    print(r.json()['data'])
+    return r.json()['data']
     # Возвращает массив со словарями
     # [{'city_id': 1, 'city_name': 'Алмазный'},
     #  {'city_id': 2, 'city_name': 'Восточный'},
@@ -28,21 +28,21 @@ def get_cities():
 def get_city_data(city_id):
     r = requests.get('http://dt.miet.ru/ppo_it/api/' + city_id,
                      headers={'X-Auth-Token': '3zqb6xwartk4fga1'})
-    # Тут вроде понятно
+    # Возвращает кортеж c элементами в порядке кол-ва домов, районов, квартир, температуры, название
     house_count = r.json()['data']['house_count']
     area_count = r.json()['data']['area_count']
     apartment_count = r.json()['data']['apartment_count']
     temperature = r.json()['data']['temperature']
     city_name = r.json()['data']['city_name']
-    print(house_count, area_count, apartment_count, temperature, city_name)
+    return house_count, area_count, apartment_count, temperature, city_name
 
 
 def get_city_temperature(city_id):
     r = requests.get('http://dt.miet.ru/ppo_it/api/' + city_id + '/temperature',
                      headers={'X-Auth-Token': '3zqb6xwartk4fga1'})
-    # Тут тоже вроде понятно
+    # Возвращает температуру в указанном городе
     temp = r.json()['data']
-    print(temp)
+    return temp
 
 
 def get_area_data(city_id, area_id):
@@ -50,15 +50,15 @@ def get_area_data(city_id, area_id):
                      headers={'X-Auth-Token': '3zqb6xwartk4fga1'})
     # Возвращает массив со словарями, в которых содержатся id домов и их кол-во квартир (В районе)
     # {'house_id': 1, 'apartment_count': 56} и т. д.
-    print(r.json()['data'])
+    return r.json()['data']
 
 
 def get_apartment_count(city_id, area_id, house_id):
     r = requests.get('http://dt.miet.ru/ppo_it/api/' + city_id + '/' + area_id + '/' + house_id,
                      headers={'X-Auth-Token': '3zqb6xwartk4fga1'})
-    # Возвращает количество квартир в доме в районе в городе ¯\_(ツ)_/¯
+    # Возвращает количество квартир в доме в районе в городе
     apartment_count = r.json()['data']
-    print(apartment_count)
+    return apartment_count
 
 
 def get_apartments_temperature(city_id, area_id, house_id):
@@ -67,16 +67,16 @@ def get_apartments_temperature(city_id, area_id, house_id):
                      headers={'X-Auth-Token': '3zqb6xwartk4fga1'})
     # Возвращает массив со словарями, в которых квартиры и температура в них
     # {'apartment_id': 1, 'temperature': 19} и т. д.
-    print(r.json()['data'])
+    return r.json()['data']
 
 
 def get_apartment_temperature(city_id, area_id, house_id, apartment_id):
     r = requests.get('http://dt.miet.ru/ppo_it/api/' + city_id + '/' +
                      area_id + '/' + house_id + '/' + apartment_id,
                      headers={'X-Auth-Token': '3zqb6xwartk4fga1'})
-    # Возвращает температуру в квартире в доме в районе в городе
+    # Возвращает температуру в квартире в доме в районе в городе ¯\_(ツ)_/¯
     temp = r.json()['data']['temperature']
-    print(temp)
+    return temp
 
 
-# Здесь должна быть функция, но её тут нет, т.к. она бесполезна
+# Здесь должна быть функция, но её тут нет, т.к. она бесполезна (Та же функция, что и верхняя)
