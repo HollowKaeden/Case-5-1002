ac61cfa7d42150995a4bbe39139b673147861cf3
RuletkaDudo <61617281+RuletkaDudo@users.noreply.github.com>
2020-03-08 03:19:28 +0300

    Update dataBase.py
    новые функции :

--- dataBase.py
+++ dataBase.py
@@ -1,5 +1,6 @@
 # Андрей
 import sqlite3
+from remote import *
 
 
 class City:
@@ -11,10 +12,11 @@ class City:
         self.apartment_count = apartment_count
 
 
+
 cursor = None
 conn = None
 
-def connect(db_name):
+def connect():
     global cursor, conn
     conn = sqlite3.connect("temp_data_full.db")
     cursor = conn.cursor()
@@ -25,6 +27,11 @@ def clearDtBs():
     cursor.execute('DELETE FROM HOUSES')
     cursor.execute('DELETE FROM CITIES')
 
+    conn.commit()
+
+def clearDtBs_temperature():
+    cursor.execute('DELETE FROM APARTMENT_TEMPERATURE')
+    cursor.execute('DELETE FROM CITIES_TEMPERATURE')
 
 def add_city(city_id, city):
     # name, area_count, house_count, apartment_count
@@ -32,6 +39,19 @@ def add_city(city_id, city):
                                                              city['city_name'], city['area_count'],
                                                              city['house_count'], city['apartment_count']))
     conn.commit()
+
+
+def add_house(city_id, area_id, house):
+    cursor.execute('''INSERT INTO HOUSES (city_id, area_id, house_number, apartment_count) 
+                                   VALUES (?,?,?,?)''',
+                                   (city_id, area_id, house['house_id'],
+                                   house['apartment_count']))
+
+
+def add_apartment(apartment_number, house):
+    cursor.execute('''INSERT INTO APARTMENTS (apartment_number, house_id) 
+                                        VALUES (?,?)''',
+                   (apartment_number, house['house_id']))
 
 
 def get_cities():
@@ -42,3 +62,28 @@ def get_cities():
 def get_city(id) -> City:
     result = cursor.execute("SELECT * FROM CITIES WHERE id=?", (id, )).fetchone()
     return City(result[0], result[1], result[2], result[3], result[4])
+
+
+def get_temperature_City_year():
+    cursor.execute('SELECT APARTMENTS (apartment_number, house_id')
+
+
+def get_names_citys():
+    return [i[0] for i in cursor.execute('SELECT name FROM CITIES')]
+
+
+def add_temperature_citys(time_id):
+    for i in [j[0] for j in cursor.execute('SELECT id FROM CITIES')]:
+        cursor.execute('INSERT INTO CITIES_TEMPERATURE (city_id, time_id, temperature) VALUES (?, ?, ?)',
+                       (i, time_id, float(get_city_temperature(i))))
+    conn.commit()
+
+
+def add_temperature_apartments(time_id):
+    for house in list(cursor.execute('SELECT * FROM HOUSES')):
+        print(2)
+        city_id, area_id, house_id = house[1], house[2], house[3]
+        for apartment_id in range(1, house[-1] + 1):
+            cursor.execute('INSERT INTO APARTMENT_TEMPERATURE (apartment_id, time_id, temperature) VALUES (?, ?, ?)',
+                           (apartment_id, time_id, float(get_apartment_temperature(city_id, area_id, house_id, apartment_id))))
+        conn.commit()
