718388de0ccbeccd6fd10aed65eae2630cf6cd1b
RuletkaDudo <61617281+RuletkaDudo@users.noreply.github.com>
2020-03-08 21:23:27 +0300

    вроде сделал

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from schedule import *
 from time import *
 import remote
+import datetime as dt
 import dataBase as db
 conn = None
 cursor = None
@@ -13,13 +14,14 @@ def job():
     time += 1
     for city in db.get_cities():
         temp = remote.get_city_temperature(city.id)
-        db.add_temperature_city(time, temp, city)
+        db.add_temperature_city(time, temp, city.id)
 
-    for house in db.get_houses_apartment_count():
+    for house in db.get_houses():
         city_id, area_id, house_id = house[1], house[2], house[3]
+        # тут вместо range сделать в dataBase функцию get_apartments_number() ?
         for apartment_id in range(1, house[-1] + 1):
             db.add_temperature_apartment(time, apartment_id, db.get_apartment_temperature(city_id, area_id, house_id, apartment_id))
-
+    conn.commit()
 
 every(8).minutes.do(job)
 
