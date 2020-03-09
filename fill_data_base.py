9ccce530646733094526e76a6f7feafa61198c59
RuletkaDudo <61617281+RuletkaDudo@users.noreply.github.com>
2020-03-10 02:19:53 +0300

    Теперь нормально все добавляет

--- fill_data_base.py
+++ fill_data_base.py
@@ -10,9 +10,9 @@ def instalInfo():
         city_id = city_null['city_id']
         city = get_city_data(city_id)
         db.add_city(city_id, city)
+
         for area_id in range(1, city['area_count'] + 1):
             area = get_area_data(city_id, area_id)
             for house in area:
                 db.add_house(city_id, area_id, house)
-                for apartment_number in range(1, house['apartment_count'] + 1):
-                    db.add_apartment(apartment_number, house)
+    db.add_apartments()
