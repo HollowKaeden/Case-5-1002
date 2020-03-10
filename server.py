997a5cffb0114046be4cae7c487b0ec8b4738d31
RuletkaDudo <61617281+RuletkaDudo@users.noreply.github.com>
2020-03-11 01:10:47 +0300

    Доделал запросы
    Я попробую на ночь включить< но думаю инет вырубиться.
    Надеюсь норм будет.

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 from schedule import *
 from time import *
-import remote
+from remote import *
 import datetime as dt
 import dataBase as db
 conn = None
 cursor = None
 db.connect()
 time = 0
-
+city_id = 11
 
 def job():
     global time
     time += 1
-    db.add_temperature_citys(time)
-    db.add_temperature_apartments(time)
-
+    print('Начал работать в:', datetime.datetime.now())
+    db.do_all_requests(time, city_id)
+    print('Закончил работать в:', datetime.datetime.now())
 
 every(8).minutes.do(job)
 while time != 180:
